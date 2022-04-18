651dff82c63651ac6afbaff2e2785a8905943469
KhameevaNatalia <khameevana@gmail.com>
2022-04-18 14:38:31 +0300

    final_end

--- task_final/solution.js
+++ task_final/solution.js
@@ -43,7 +43,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
         data.goods[i] = {title: goods[i].title, count: goods[i].count};
-        data.goods.push    
+           
     }
     data.client = name + " " + phone;
     address = `ул. ` + address.street + `, дом ` + address.house + `, ` + address.entrance + ` подъезд, ` + address.floor + ` этаж, кв ` + address.flat;
